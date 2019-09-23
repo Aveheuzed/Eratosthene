523601058192df856cad5dcf07e725170fd5d05e
Aveheuzed <a.masson555@ntymail.com>
2019-09-23 18:33:55 +0200

    First commit ; multiplication de la vitesse de calcul par ~5 et réduction de la consommation de mémoire

--- listofbool.py
+++ listofbool.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python3
 from time import perf_counter_ns
 
-def ceil(D, d) :
-    # returns ceil(D/d), without using floats
-    q,r = divmod(D, d)
-    if not r :
-        return q
-    return q+1
+import numpy as np
 
 bornesup = int(input("Borne supérieure de recherche (exclue) : "))
 maxsearch = int(bornesup**0.5)
 
-nombres = [False,] * bornesup # False pour "pas barré"
+nombres = np.zeros(bornesup, dtype='bool') # False pour "pas barré"
 p = 2
 
 a = perf_counter_ns()
 
 while p < maxsearch :
-    nombres[p**2::p] = (True,) * ceil(bornesup-p**2,p)
-    p = nombres.index(False, p+1)
+    nombres[p**2::p] = True
+    p += 1
+    p = np.argmin(nombres[p:])+p
 
 b = perf_counter_ns()
 
-print(f"Il y a {nombres.count(False)-2:_} nombres premiers < {bornesup:_}")
+print(f"Il y a {bornesup-nombres.sum()-2:_} nombres premiers < {bornesup:_}")
 # -2 parce qu'on a inclus 0 et 1 dans le comptage
 print(f"Temps d'exécution : {b-a:_} ns")
 del nombres
